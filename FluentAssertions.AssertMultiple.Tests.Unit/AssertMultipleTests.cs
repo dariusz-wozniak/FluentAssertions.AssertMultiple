64a5541ef7fc91d4bb3095d04f0f8be3d5301201
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2019-09-04 20:08:12 +0200

    feat: add AssertMultiple
    NOTE: along with initializing repo 🎉

---
+++ FluentAssertions.AssertMultiple.Tests.Unit/AssertMultipleTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace FluentAssertions.AssertMultiple.Tests.Unit
+{
+    public class AssertMultipleTests
+    {
+        private const string Newline = "\r\n";
+
+        [Test]
+        public void AssertionShouldBeRunMultipleTimes()
+        {
+            try
+            {
+                AssertMultiple.Multiple(() =>
+                {
+                    (2 + 2).Should().Be(5);
+                    (2 + 2).Should().Be(6);
+                });
+            }
+            catch (AssertionException exception)
+            {
+                exception.Message
+                    .Should()
+                    .Be($"Expected (2 + 2) to be 5, but found 4.{Newline}Expected (2 + 2) to be 6, but found 4.{Newline}");
+            }
+        }
+    }
+}
---
+++ FluentAssertions.AssertMultiple/AssertMultiple.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using FluentAssertions.Execution;
+
+namespace FluentAssertions.AssertMultiple
+{
+    public class AssertMultiple
+    {
+        public static void Multiple(Action act)
+        {
+            using (new AssertionScope()) act();
+        }
+    }
+}
